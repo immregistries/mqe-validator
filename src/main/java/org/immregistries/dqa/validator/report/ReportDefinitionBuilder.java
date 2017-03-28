0388180f0303140e704be16535877835582aa0c8
Josh Hull <j.hull@kunzleigh.com>
2017-03-28 16:43:01 -0400

    Started the Vaccination Section of the Report

--- src/main/java/org/immregistries/dqa/validator/report/ReportDefinitionBuilder.java
+++ src/main/java/org/immregistries/dqa/validator/report/ReportDefinitionBuilder.java
@@ -23,51 +23,79 @@ public enum ReportDefinitionBuilder {
 	public ReportDefinition getDeafult() {
 		ReportDefinition def = new ReportDefinition();
 		
-		//build a section for the patient. 
-		ReportCompletenessSectionDefinition section = new ReportCompletenessSectionDefinition();
-		section.setLabel("Patient");
-		section.setSectionObject(IssueObject.PATIENT);
-		section.setReportFields(getPatientReportFields());
-
-		def.getQualitySections().add(section);
+		def.getQualitySections().add(getDefaultPatientSection());
+		def.getQualitySections().add(getDefaultVaccinationSection());
 		return def;
+	}
+	
+	
+	public ReportCompletenessSectionDefinition getDefaultVaccinationSection() {
+		//build a section for the patient. 
+				ReportCompletenessSectionDefinition section = new ReportCompletenessSectionDefinition();
+				section.setLabel("Vaccination");
+				section.setSectionObject(IssueObject.VACCINATION);
+				section.setReportFields(getVaccinationReportFields());
+
+				return section;
+	}
+	
+	public ReportCompletenessSectionDefinition getDefaultPatientSection() {
+		//build a section for the patient. 
+				ReportCompletenessSectionDefinition section = new ReportCompletenessSectionDefinition();
+				section.setLabel("Patient");
+				section.setSectionObject(IssueObject.PATIENT);
+				section.setReportFields(getPatientReportFields());
+
+				return section;
+	}
+	public List<DqaReportFieldDefinition> getVaccinationReportFields() {
+		List<DqaReportFieldDefinition> fields = new ArrayList<>();
+		boolean checkForMissing = true;
+		boolean dontCheckForMissing = true;
+		fields.add(generateFieldDefinition("VACCINATION ADMIN DATE", Requirement.RECCOMENDED, VxuField.VACCINATION_ADMIN_DATE, 20, checkForMissing, getDefaultFieldIssues()));
+		return fields;
 	}
 	
 	public List<DqaReportFieldDefinition> getPatientReportFields() {
 		List<DqaReportFieldDefinition> fields = new ArrayList<>();
+		boolean checkForMissing = true;
+		boolean dontCheckForMissing = true;
 		//here's the list: 
 	//Required
 		//patient-id 40
-		fields.add(generateFieldDefinition("Patient Id", Requirement.REQUIRED, VxuField.PATIENT_SUBMITTER_ID, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Patient Id", Requirement.REQUIRED, VxuField.PATIENT_SUBMITTER_ID, 30, checkForMissing, getDefaultFieldIssues()));
 		//first name 20
-		fields.add(generateFieldDefinition("First Name", Requirement.REQUIRED, VxuField.PATIENT_NAME_FIRST, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("First Name", Requirement.REQUIRED, VxuField.PATIENT_NAME_FIRST, 20, checkForMissing, getDefaultFieldIssues()));
 		//last name  20
-		fields.add(generateFieldDefinition("Last Name", Requirement.REQUIRED, VxuField.PATIENT_NAME_LAST, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Last Name", Requirement.REQUIRED, VxuField.PATIENT_NAME_LAST, 20, checkForMissing, getDefaultFieldIssues()));
 		//patient name - no default issues. dont' check presense.  100pts?
 			//Possible Test Name 1x
 			//Possible Baby Name 1x
 		//Birth Date
-		fields.add(generateFieldDefinition("Birth Date", Requirement.REQUIRED, VxuField.PATIENT_BIRTH_DATE, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Birth Date", Requirement.REQUIRED, VxuField.PATIENT_BIRTH_DATE, 20, checkForMissing, getDefaultFieldIssues()));
 		//gender
-		fields.add(generateFieldDefinition("Sex", Requirement.REQUIRED, VxuField.PATIENT_GENDER, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Sex", Requirement.REQUIRED, VxuField.PATIENT_GENDER, 20, checkForMissing, getDefaultFieldIssues()));
 		//Complete Address
-		fields.add(generateFieldDefinition("Complete Address", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Complete Address", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS, 20, checkForMissing, getDefaultFieldIssues()));
 		//street
-		fields.add(generateFieldDefinition("Street", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS_STREET, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Street", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS_STREET, 20, checkForMissing, getDefaultFieldIssues()));
 		//city
-		fields.add(generateFieldDefinition("City", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS_CITY, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("City", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS_CITY, 20, checkForMissing, getDefaultFieldIssues()));
 		//state
-		fields.add(generateFieldDefinition("State", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS_STATE, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("State", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS_STATE, 20, checkForMissing, getDefaultFieldIssues()));
 		//zip
-		fields.add(generateFieldDefinition("Zip", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS_ZIP, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Zip", Requirement.REQUIRED, VxuField.PATIENT_ADDRESS_ZIP, 20, checkForMissing, getDefaultFieldIssues()));
 
 		//expected
 		//middle name
-		fields.add(generateFieldDefinition("Middle Name", Requirement.EXPECTED, VxuField.PATIENT_MIDDLE_NAME, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Middle Name", Requirement.EXPECTED, VxuField.PATIENT_MIDDLE_NAME, 20, checkForMissing, getDefaultFieldIssues()));
 		//phone
-		fields.add(generateFieldDefinition("Phone", Requirement.EXPECTED, VxuField.PATIENT_PHONE, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Phone", Requirement.EXPECTED, VxuField.PATIENT_PHONE, 20, checkForMissing, getDefaultFieldIssues()));
 		//mother's maiden
-		fields.add(generateFieldDefinition("Mother's Maiden", Requirement.EXPECTED, VxuField.PATIENT_MOTHERS_MAIDEN_NAME, 20, true, getDefaultFieldIssues()));
+		fields.add(generateFieldDefinition("Mother's Maiden", Requirement.EXPECTED, VxuField.PATIENT_MOTHERS_MAIDEN_NAME, 20, checkForMissing, getDefaultFieldIssues()));
+		
+		fields.add(generateFieldDefinition("Ethnicity", Requirement.RECCOMENDED, VxuField.PATIENT_ETHNICITY, 20, checkForMissing, getDefaultFieldIssues()));
+		
 		return fields;
 	}
 	
@@ -81,7 +109,7 @@ public enum ReportDefinitionBuilder {
 		def.setRequirement(requirement);
 		
 		return def;
-	}
+	}	
 	
 	public List<ReportIssue> getDefaultFieldIssues() {
 		List<ReportIssue> defaults = new ArrayList<>();

bdbb09dd4925ccbb60ada3d330726b52786477ee
Josh Hull <j.hull@kunzleigh.com>
2018-03-22 17:34:00 -0400

    Refactor ValidationIssue -> rename -> ValidationReport + Code Formatter

--- src/main/java/org/immregistries/dqa/validator/detection/ValidationReport.java
+++ src/main/java/org/immregistries/dqa/validator/detection/ValidationReport.java
@@ -1,5 +1,7 @@
 package org.immregistries.dqa.validator.detection;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.immregistries.dqa.hl7util.Reportable;
 import org.immregistries.dqa.hl7util.ReportableSource;
@@ -9,120 +11,110 @@ import org.immregistries.dqa.hl7util.model.Hl7Location;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+public class ValidationReport implements Reportable {
 
-public class ValidationDetection implements Reportable
-{
-  
-private static final Logger logger = LoggerFactory
-		.getLogger(ValidationDetection.class);
+  private static final Logger logger = LoggerFactory.getLogger(ValidationReport.class);
 
-  private Detection detection = null;//should this be a String?
-  private int positionId = 0;//This says where in the ACK to put it. 
-  private SeverityLevel severityLevel = null; //this is how bad it is. 
-  private String valueReceived = null;//This is the related value.
+  private Detection detection = null;// should this be a String?
+  private int positionId = 0;// This says where in the ACK to put it.
+  private SeverityLevel severityLevel = null; // this is how bad it is.
+  private String valueReceived = null;// This is the related value.
   private List<Hl7Location> hl7LocationList = new ArrayList<>();
-  
-  public Detection getDetection()
-  {
+
+  public Detection getDetection() {
     return detection;
   }
-  
+
   public String getMessage() {
-	  return detection != null ? detection.getDisplayText() : "";
+    return detection != null ? detection.getDisplayText() : "";
   }
-  
-  public void setDetection(Detection issue)
-  {
+
+  public void setDetection(Detection issue) {
     this.detection = issue;
   }
-  public int getPositionId()
-  {
+
+  public int getPositionId() {
     return positionId;
   }
-  public void setPositionId(int positionId)
-  {
+
+  public void setPositionId(int positionId) {
     this.positionId = positionId;
   }
-  
-  public void setSeverityLevel(SeverityLevel level)
-  {
+
+  public void setSeverityLevel(SeverityLevel level) {
     this.severityLevel = level;
   }
-  public String getValueReceived()
-  {
+
+  public String getValueReceived() {
     return valueReceived;
   }
-  public void setValueReceived(String valueReceived)
-  {
+
+  public void setValueReceived(String valueReceived) {
     this.valueReceived = valueReceived;
   }
 
-  public boolean isError()
-  {
-	  return (this.severityLevel != null && SeverityLevel.ERROR.equals(this.severityLevel))
+  public boolean isError() {
+    return (this.severityLevel != null && SeverityLevel.ERROR.equals(this.severityLevel))
         || this.severityLevel == null && this.detection.getSeverity() == SeverityLevel.ERROR;
   }
-  
+
 
   @Override
   public ReportableSource getSource() {
     return ReportableSource.DQA;
   }
 
-@Override
-public String toString() {
-	return "IssueFound [issue=" + detection + ", positionId=" + positionId
-			+ ", severity=" + getSeverity() + ", codeReceived=" + valueReceived
-			+ "]";
-}
+  @Override
+  public String toString() {
+    return "IssueFound [issue=" + detection + ", positionId=" + positionId + ", severity="
+        + getSeverity() + ", codeReceived=" + valueReceived + "]";
+  }
 
-@Override
-public SeverityLevel getSeverity() {
-	if (this.severityLevel == null && this.detection != null) {
-		return this.detection.getSeverity();
-	}
-	return this.severityLevel;
-}
+  @Override
+  public SeverityLevel getSeverity() {
+    if (this.severityLevel == null && this.detection != null) {
+      return this.detection.getSeverity();
+    }
+    return this.severityLevel;
+  }
 
-@Override
-public CodedWithExceptions getHl7ErrorCode() {
-	CodedWithExceptions cwe = new CodedWithExceptions();
-	cwe.setIdentifier(this.detection.getHl7ErrorCode());
-	return cwe;
-}
+  @Override
+  public CodedWithExceptions getHl7ErrorCode() {
+    CodedWithExceptions cwe = new CodedWithExceptions();
+    cwe.setIdentifier(this.detection.getHl7ErrorCode());
+    return cwe;
+  }
 
-@Override
-public List<Hl7Location> getHl7LocationList() {
-	return this.hl7LocationList;
-}
+  @Override
+  public List<Hl7Location> getHl7LocationList() {
+    return this.hl7LocationList;
+  }
 
-@Override
-public String getReportedMessage() {
-	return this.detection.getDisplayText();
-}
+  @Override
+  public String getReportedMessage() {
+    return this.detection.getDisplayText();
+  }
 
-@Override
-public String getDiagnosticMessage() {
-	if (!StringUtils.isBlank(this.valueReceived)) {
-		return "Value Received: [" + this.valueReceived + "]";
-	}
-	return null;
-}
+  @Override
+  public String getDiagnosticMessage() {
+    if (!StringUtils.isBlank(this.valueReceived)) {
+      return "Value Received: [" + this.valueReceived + "]";
+    }
+    return null;
+  }
 
-@Override
-public CodedWithExceptions getApplicationErrorCode() {
-	CodedWithExceptions cwe = new CodedWithExceptions();
-	if (!this.detection.getApplicationErrorCode().equals("")){
-	  cwe.setIdentifier(this.detection.getApplicationErrorCode().getId());
+  @Override
+  public CodedWithExceptions getApplicationErrorCode() {
+    CodedWithExceptions cwe = new CodedWithExceptions();
+    if (!this.detection.getApplicationErrorCode().equals("")) {
+      cwe.setIdentifier(this.detection.getApplicationErrorCode().getId());
       cwe.setText(this.detection.getApplicationErrorCode().getText());
       cwe.setNameOfCodingSystem("HL70533");
       cwe.setAlternateIdentifier(this.detection.getDqaErrorCode());
       cwe.setAlternateText(this.detection.getDisplayText());
       cwe.setNameOfAlternateCodingSystem("L");
-	}
-	return cwe;
-}
+    }
+    return cwe;
+  }
 
 }

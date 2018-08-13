f811f0ade0015b917086a395310bf555889c962e
Josh Hull <j.hull@kunzleigh.com>
2018-08-13 14:40:18 -0600

    MQE-49 Patient Birth Date

--- src/main/java/org/immregistries/mqe/validator/engine/rules/patient/PatientBirthDateIsValid.java
+++ src/main/java/org/immregistries/mqe/validator/engine/rules/patient/PatientBirthDateIsValid.java
@@ -7,24 +7,21 @@ import org.immregistries.mqe.validator.detection.Detection;
 import org.immregistries.mqe.validator.detection.ValidationReport;
 import org.immregistries.mqe.validator.engine.ValidationRule;
 import org.immregistries.mqe.validator.engine.ValidationRuleResult;
+import org.immregistries.mqe.validator.engine.rules.header.MessageHeaderDateIsValid;
 import org.immregistries.mqe.vxu.MqeMessageReceived;
 import org.immregistries.mqe.vxu.MqePatient;
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
 
 public class PatientBirthDateIsValid extends ValidationRule<MqePatient> {
 
   @Override
   protected final Class[] getDependencies() {
-    return new Class[] {PatientExists.class};
+    return new Class[]{PatientExists.class
+    };
   }
 
   public PatientBirthDateIsValid() {
     this.ruleDetections.addAll(Arrays.asList(Detection.PatientBirthDateIsMissing,
-        Detection.PatientBirthDateIsInvalid, Detection.PatientBirthDateIsInFuture,
-        Detection.PatientBirthDateIsOn15ThDayOfMonth, Detection.PatientBirthDateIsOnFirstDayOfMonth,
-        Detection.PatientBirthDateIsOnLastDayOfMonth,
-        Detection.PatientBirthDateIsAfterSubmission));
+        Detection.PatientBirthDateIsInvalid));
   }
 
   @Override
@@ -40,40 +37,6 @@ public class PatientBirthDateIsValid extends ValidationRule<MqePatient> {
     } else if (!this.common.isValidDate(birthDateString)) {
       issues.add(Detection.PatientBirthDateIsInvalid.build((birthDateString), target));
       passed = false;
-    } else {
-      DateTime birthDate = this.common.parseDateTimeFrom(birthDateString);
-
-      // in the original validator, the "future" was determined based
-      // on when the message is validated... we might need to keep that.
-      DateTime receivedDate = new DateTime(message.getReceivedDate());
-
-      if (receivedDate != null && receivedDate.isBefore(birthDate)) {
-        issues.add(Detection.PatientBirthDateIsInFuture.build((birthDateString), target));
-        passed = false;
-      }
-
-      DateTime messageDate = new DateTime(message.getMessageHeader().getMessageDate());
-
-      if (messageDate != null && messageDate.isBefore(birthDate)) {
-        issues.add(Detection.PatientBirthDateIsAfterSubmission.build((birthDateString), target));
-        passed = false;
-      }
-      
-   // After this, we have a date.
-      int dayOfMonth = birthDate.getDayOfMonth();
-
-      LocalDate lastDayOfMonth = birthDate.toLocalDate().dayOfMonth().withMaximumValue();
-
-      int lastDay = lastDayOfMonth.getDayOfMonth();
-
-      if (dayOfMonth == 1) {
-        issues.add(Detection.PatientBirthDateIsOnFirstDayOfMonth.build((birthDateString), target));
-      } else if (dayOfMonth == 15) {
-        issues.add(Detection.PatientBirthDateIsOn15ThDayOfMonth.build((birthDateString), target));
-      } else if (dayOfMonth == lastDay) {
-        issues.add(Detection.PatientBirthDateIsOnLastDayOfMonth.build((birthDateString), target));
-      }
-
     }
 
     return buildResults(issues, passed);
